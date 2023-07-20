42e9c67e1ea8d251105a169c223e637ae487fb57
A-Steeves <48775637+AlexSteeves@users.noreply.github.com>
2023-07-20 13:58:50 -0400

    Com 1

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+      title: "TailwindCSS Practice",
+      subtitle: "Tailwind and React",
+      description:
+        "Used TailwindCSS and React to replicate discords sidebar",
+      image: "./TailWind-Discord-Practice.png",
+      link: "https://github.com/AlexSteeves/tailwind-practice",
+    },
+    
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://reactbootcamp.com",
+    },
+  ];
+  
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "Next.js",
+    "Node",
+    "Java",
+    
+  ];
+  
