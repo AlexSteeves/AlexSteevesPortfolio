6ac8ec328137c515d287db7920e0ec8eb4aeefec
A-Steeves <48775637+AlexSteeves@users.noreply.github.com>
2023-07-25 14:13:30 -0400

    fixes

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef} from "react";
 import { RxDotFilled } from "react-icons/rx";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
@@ -35,11 +35,23 @@ export default function Carousel() {
     setCurrentIndex(slideIndex);
   };
 
+
+  const [imageHeight, setImageHeight] = useState(0);
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    // Get the height of the image after it has loaded
+    if (imageRef.current) {
+      setImageHeight(imageRef.current.clientHeight);
+    }
+  }, [currentIndex]);
+
   return (
-    <div className="h-1/2 w-full m-auto  my-0 px-4 relative group rounded-xl border-gray-900 border-1">
+    <div className="h-2/3 w-full md:h-1/2 md:w-1/2 m-auto  my-0 px-4 relative group rounded-xl border-gray-900 border-1">
    
       <div
         style={{
+          
           backgroundSize: "contain",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -48,13 +60,16 @@ export default function Carousel() {
       >
         <a href={projects[currentIndex].link}>
           <img
+            ref ={imageRef}
             src={projects[currentIndex].image}
             alt="gallery"
-            className="absolute inset-0 object-contain object-center w-full h-full transition-all rounded-xl drop-shadow-sm border-b-gray-900 border-r-1"
+            className="absolute inset-0 object-contain object-center w-full  transition-all rounded-xl drop-shadow-sm border-b-gray-900 border-r-1"
           />
 
-          <div className="absolute inset-0 hover:bg-gray-700 opacity-0 hover:opacity-100 border-gray-900 border-5 transition-opacity  object-contain duration-500  bg-gray-900 ease in rounded-xl ">
-            <div className="px-8 md:py-10 h-contain flex flex-col items-center justify-center text-center text-black">
+          <div style = {{height: imageHeight}} className="absolute inset-0 hover:bg-gray-700 opacity-0 hover:opacity-100 border-gray-900 border-5 transition-opacity object-contain duration-500 bg-gray-900 ease-in rounded-xl">
+
+
+            <div className="px-8 md:py-10 flex flex-col items-center justify-center text-center text-black h-full">
               <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                 {projects[currentIndex].subtitle}
               </h2>
@@ -65,13 +80,8 @@ export default function Carousel() {
                 {projects[currentIndex].description}
               </p>
             </div>
-          </div>
-        </a>
-      
-      </div>
-
-      {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+            {/* Left Arrow */}
+      <div className=" group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactLeft onClick={previousSlide} size={30} />
       </div>
 
@@ -79,6 +89,12 @@ export default function Carousel() {
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer opacity-80">
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
+          </div>
+        </a>
+      
+      </div>
+
+      
     </div>
   );
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,7 +6,7 @@ import Carousel from './Carousel';
 export default function Projects() {
   return (
     <section id="projects" className="text-gray-800 flex flex-col bg-gray-900 body-font w-screen h-screen items-center justify-center">
-      <div className="container px-5 md:py-5 mx-auto text-center lg:px-40 items-center justify-center">
+      <div className="container pt-52 px-5 md:py-5 mx-auto text-center lg:px-40 items-center justify-center">
         <div className="flex flex-col w-full">
           <CodeIcon className="mx-auto inline-block w-10" />
           <h1 className=" text-3xl font-medium title-font text-bg-800">
