6a6d1d93edbba423c5a605ca65cb6eb64caaae2c
AlexSteeves <48775637+AlexSteeves@users.noreply.github.com>
2023-08-08 10:36:06 -0400

    Added copyright

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from "./Components/Contact";
 import Navbar from "./Components/Navbar";
 import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
+import Footer from "./Components/Footer";
 
 export default function App() {
   return (
@@ -28,6 +29,9 @@ export default function App() {
 
       <div className="h-screen w-screen snap-start snap-always ">
         <Skills />
+      </div>
+      <div className="h-1/8 w-screen snap-start snap-always">
+        <Footer />
       </div>
 
       
---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+
+
+export default function Footer() {
+
+    return(
+        <div className = "w-screen h-1/5 bg-gray-700">
+            <h1 className = "text-gray-800 p-2 text-center justify-center">@Copyright 2023</h1>
+        </div>
+    );
+}
